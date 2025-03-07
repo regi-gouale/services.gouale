7a5b0291ea8b6d84d565411937c24d5d49d7d462
Régi GOUALE <regi.gouale@gmail.com>
2025-03-07 11:41:12 +0100

    Ajout de la gestion des en-têtes CORS et de la politique de sécurité du contenu dans la configuration Next.js.

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,24 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "Content-Security-Policy",
+            value:
+              "default-src 'self'; connect-src 'self' https://services.gouale.com; frame-ancestors 'none'; img-src 'self' https://images.unsplash.com; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline';",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
